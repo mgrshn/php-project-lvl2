9f1456f9920045a0c2658205776bdfcbd3b583c0
Grishin <grishinma2009@yandex.ru>
2022-05-04 21:46:19 +0300

    created comparator (Differ\genDiff()), that can compare two plain json files and show their differences

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/mgrshn/php-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/mgrshn/php-project-lvl2/actions)
+Comparing 2 plane json files:
+[![asciicast](https://asciinema.org/a/WqJbPVioRizE6AakIPwkOb9pP.svg)](https://asciinema.org/a/WqJbPVioRizE6AakIPwkOb9pP)
---
+++ src/Differ.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Differ\Differ;
+
+function genDiff($pathToFile1, $pathToFile2): string
+{
+    if(!file_exists($pathToFile1)) {
+        $pathToFile1 = __DIR__ . "/../fixtures/{$pathToFile1}";
+    }
+    if(!file_exists($pathToFile2)) {
+        $pathToFile2 = __DIR__ . "/../fixtures/{$pathToFile2}";
+    }
+    $firstFileAsArray = json_decode(file_get_contents($pathToFile1), true);
+    $secondFileAsArray = json_decode(file_get_contents($pathToFile2), true);
+    $mergedArray = array_merge_recursive($firstFileAsArray, $secondFileAsArray);
+    ksort($mergedArray);
+    $resultArr = [];
+    foreach ($mergedArray as $key => $value) {
+        if (is_array($value) && $value[0] === $value[1]) {
+            $resultArr["  {$key}"] = $value[0];
+        } elseif (is_array($value) && $value[0] !== $value[1]) {
+            $resultArr["- {$key}"] = $value[0];
+            $resultArr["+ {$key}"] = $value[1];
+        } elseif (!array_key_exists($key, $secondFileAsArray)) {
+            $resultArr["- {$key}"] = $value;
+        } else {
+            $resultArr["+ {$key}"] = $value;
+        }
+    }
+    $result = str_replace('"', '', json_encode($resultArr));
+    $result = str_replace('{', "{\n  ", $result);
+    $result = str_replace('}', "\n}", $result);
+    $result = str_replace(",", "\n  ", $result);
+    $result = str_replace(":", ": ", $result);
+    return $result;
+}
