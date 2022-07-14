ee7fe2864fcc0bc5bdc33cffa8e25f8bc79baa64
Grishin <grishinma2009@yandex.ru>
2022-07-15 01:08:53 +0300

    Revert "Revert "added plain formatter, that can return comparing as a text""
    This reverts commit 52dc3a57e3bbb4de0cd69b6f157992ac3fe376a6.

--- src/Differ.php
+++ src/Differ.php
@@ -6,6 +6,7 @@ use function Differ\Parser\parse;
 use function Differ\Builder\build;
 use function Differ\Formatter\stylish;
 use function Differ\Formatter\toString;
+use function Differ\Formatter\plain;
 
 function genDiff(string $pathToFile1, string $pathToFile2, string $format)
 {
@@ -16,6 +17,8 @@ function genDiff(string $pathToFile1, string $pathToFile2, string $format)
     if ($format === 'stylish') {
         $formattedFilesTree = stylish($files);
         $result = toString($formattedFilesTree);
+    } elseif ($format === 'plain') {
+        $result = plain($files);
     }
 
     return $result;
