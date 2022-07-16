10478b870364b9b121efee7125d9fb777a1fc466
Grishin <grishinma2009@yandex.ru>
2022-07-16 21:28:47 +0300

    cosmetic changes for linter;

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -2,19 +2,18 @@
 
 namespace Differ\Tests;
 
-
 require_once __DIR__ . '/../vendor/autoload.php';
 use PHPUnit\Framework\TestCase;
 use function Differ\Differ\genDiff;
 
 class DifferTest extends TestCase
 {
-    public function testGenDiff(): void 
+    public function testGenDiff(): void
     {
         $file1 = 'file1.json';
         $file2 = 'file2.json';
 
-        $result= file_get_contents(__DIR__ . '/fixtures/diffs.txt');
+        $result = file_get_contents(__DIR__ . '/fixtures/diffs.txt');
         $this->assertEquals($result, genDiff($file1, $file2, 'stylish'));
     }
 
@@ -23,7 +22,7 @@ class DifferTest extends TestCase
         $file1 = 'file1.yml';
         $file2 = 'file2.yml';
 
-        $result= file_get_contents(__DIR__ . '/fixtures/diffs.txt');
+        $result = file_get_contents(__DIR__ . '/fixtures/diffs.txt');
         $this->assertEquals($result, genDiff($file1, $file2, 'stylish'));
     }
 
