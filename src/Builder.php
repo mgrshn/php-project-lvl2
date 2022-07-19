6b53e15d0b2b24912d22853a9e295f5d4b8de5cf
Grishin <grishinma2009@yandex.ru>
2022-07-19 19:32:05 +0300

    test changes

--- src/Builder.php
+++ src/Builder.php
@@ -8,10 +8,11 @@ function build(object $firstFile, object $secondFile): array
         array_keys(get_object_vars($firstFile)),
         array_keys(get_object_vars($secondFile))
     ));
+    print_r($keys);
     sort($keys);
 
     $files = [];
-    foreach ($keys as $key) {
+    /*foreach ($keys as $key) {
         if (
             property_exists($firstFile, $key) &&
             property_exists($secondFile, $key) &&
@@ -55,6 +56,54 @@ function build(object $firstFile, object $secondFile): array
                 ];
             }
         }
-    }
-    return $files;
+    }*/
+
+    $tree = array_map(function ($key) use ($firstFile, $secondFile) {
+        if (
+            property_exists($firstFile, $key) &&
+            property_exists($secondFile, $key) &&
+            $secondFile->$key === $firstFile->$key //here. Changed non-stricted comparision to a strict one. It Works!
+        ) {
+            return [
+                'name' => $key,
+                'status' => 'not changed',
+                'value' => $secondFile->$key
+            ];
+        } elseif (!property_exists($firstFile, $key)) {
+            return [
+                'name' => $key,
+                'status' => 'added',
+                'value' => $secondFile->$key
+            ];
+        } elseif (!property_exists($secondFile, $key)) {
+            return [
+                'name' => $key,
+                'status' => 'removed',
+                'value' => $firstFile->$key
+            ];
+        } elseif (
+            property_exists($firstFile, $key) &&
+            property_exists($secondFile, $key) &&
+            $secondFile->$key !== $firstFile->$key && //here. Changed non-stricted comparision to a strict one.
+            !(is_object($firstFile->$key)) || !(is_object($secondFile->$key))
+        ) {
+            return [
+                'name' => $key,
+                'status' => 'changed',
+                'newValue' => $secondFile->$key,
+                'oldValue' => $firstFile->$key
+            ];
+        } else {
+            if (is_object($firstFile->$key) && is_object($secondFile->$key)) {
+                return [
+                    'name' => $key,
+                    'status' => 'nested',
+                    'child' => build($firstFile->$key, $secondFile->$key)
+                ];
+            }
+        }
+    }, $keys);
+    //print_r($tree);
+    //print_r($files);
+    return $tree;
 }
