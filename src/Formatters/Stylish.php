bdf75e1473d33da83623720e814ea843e63cee27
Grishin <grishinma2009@yandex.ru>
2022-08-05 01:03:23 +0300

    test

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -6,33 +6,28 @@ use Functional;
 
 function stylish(array $treeOfFiles): array
 {
-    $res = array_reduce($treeOfFiles, function ($accc, $node) {
+    $res = array_reduce($treeOfFiles, function ($acc, $node) {
         switch ($node) {
             case $node['status'] === 'not changed':
                 $acc["{$node['name']}"] = $node["value"];//Should not use of mutating operators
-                $accc = array_merge($accc, $acc);
                 break;
             case $node['status'] === 'added':
                 $acc["+ {$node['name']}"] = $node["value"];//Should not use of mutating operators
-                $accc = array_merge($accc, $acc);
                 break;
             case $node['status'] === 'removed':
                 $acc["- {$node['name']}"] = $node["value"];//Should not use of mutating operators
-                $accc = array_merge($accc, $acc);
                 break;
             case $node['status'] === 'changed':
                 $acc["- {$node['name']}"] = $node["oldValue"];//Should not use of mutating operators
                 $acc["+ {$node['name']}"] = $node["newValue"];//Should not use of mutating operators
-                $accc = array_merge($accc, $acc);
                 break;
             case $node['status'] === 'nested':
                 $acc["{$node['name']}"] = stylish($node['child']);//Should not use of mutating operators
-                $accc = array_merge($accc, $acc);
                 break;
         }
         //return $newAcc;
-        return $accc;
-    }, []);
+        return $acc;
+    });
     //print_r($res);
     return $res;
 }
