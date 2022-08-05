308f115b0f55d8f2f7a97ca172ba18574513851e
Grishin <grishinma2009@yandex.ru>
2022-08-05 03:42:48 +0300

    test

--- src/Parser.php
+++ src/Parser.php
@@ -6,21 +6,23 @@ use Symfony\Component\Yaml\Yaml;
 
 function parse(string $pathToFile)
 {
-    //if (!file_exists($pathToFile)) {
-    //    $pathToFile = __DIR__ . "/../tests/fixtures/{$pathToFile}";
-    //}
-    $pathInfo = pathinfo($pathToFile);
+    if (!file_exists($pathToFile)) {
+        $path = __DIR__ . "/../tests/fixtures/{$pathToFile}";
+    } else {
+        $path = $pathToFile;
+    }
+    $pathInfo = pathinfo($path);
     $extension = $pathInfo['extension'];
     switch ($extension) {
         default:
             $parsedFile = 'Incorrect way!';
             break;
         case 'json':
-            $parsedFile = json_decode((string) file_get_contents($pathToFile));
+            $parsedFile = json_decode((string) file_get_contents($path));
             break;
         case 'yaml':
         case 'yml':
-            $parsedFile = Yaml::parse((string) file_get_contents($pathToFile), Yaml::PARSE_OBJECT_FOR_MAP);
+            $parsedFile = Yaml::parse((string) file_get_contents($path), Yaml::PARSE_OBJECT_FOR_MAP);
             break;
     }
     return $parsedFile;
